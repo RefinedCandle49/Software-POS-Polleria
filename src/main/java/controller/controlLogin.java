c4937adbb1fd3ded93cc66ed3757add4be638e6a
DaikoDev <cdanieleon28@gmail.com>
2024-05-02 23:48:00 -0500

    Se agrego logica de inicio de sesion

---
+++ src/main/java/controller/controlLogin.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.UsuarioDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import model.Usuario;
+
+/**
+ *
+ * @author daniel
+ */
+@WebServlet(name = "controlLogin", urlPatterns = {"/controlLogin"})
+public class controlLogin extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        // Validación para administradores
+        UsuarioDao daoUsuario = new UsuarioDao();
+        Usuario usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setPassword(password);
+
+        if (daoUsuario.validarUsuario(usuario)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("usuario", usuario);
+
+            String rol = usuario.getRol();
+            if ("Administrador".equals(rol)) {
+                response.sendRedirect(request.getContextPath() + "/admin/usuarios.jsp");
+            } else if ("Cajero".equals(rol)) {
+                response.sendRedirect(request.getContextPath() + "/caja/menu.jsp");
+            } else if ("Almacenero".equals(rol)) {
+                response.sendRedirect(request.getContextPath() + "/almacen/productos.jsp");
+            }
+            return;
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}

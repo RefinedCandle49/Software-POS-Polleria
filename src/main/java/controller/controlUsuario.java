fd8d3b0546153415d9a03b1ba905ed7c7b2bd4fc
Kevin Valdez Vargas <valdezvargaskevin@gmail.com>
2024-05-03 22:06:20 +0000

    Se agregó el registrar usuarios al sistema - Miguel

---
+++ src/main/java/controller/controlUsuario.java
@@ -0,0 +1,101 @@
+
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import dao.UsuarioDao;
+import model.Usuario;
+
+/**
+ *
+ * @author user
+ */
+@WebServlet(name = "controlUsuario", urlPatterns = {"/controlUsuario"})
+public class controlUsuario extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String action = request.getParameter("action");
+
+        switch (action) {
+            case "registrar":
+                try {
+                String emailRegistrar = request.getParameter("email");
+                String passwordRegistrar = request.getParameter("password");
+                String rolRegistrar = request.getParameter("rol");
+                int estadoRegistrar = Integer.parseInt(request.getParameter("estado"));
+
+                Usuario userRegistrar = new Usuario();
+                userRegistrar.setEmail(emailRegistrar);
+                userRegistrar.setPassword(passwordRegistrar);
+                userRegistrar.setRol(rolRegistrar);
+                userRegistrar.setEstado(estadoRegistrar);
+
+                int resultRegistrar = UsuarioDao.registrarUsuario(userRegistrar);
+
+                if (resultRegistrar > 0) {
+                    response.sendRedirect(request.getContextPath() + "/admin/usuarios.jsp");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/main/webapp/admin/registrar.jsp
+++ src/main/webapp/admin/registrar.jsp
@@ -1,16 +1,47 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: RefinedCandle49
-  Date: 1/05/2024
-  Time: 20:56
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+<%@page import="model.Usuario, dao.UsuarioDao, java.util.*" %>
+<!DOCTYPE html>
 <html>
 <head>
-    <title>Title</title>
+    <title>Registro de Usuario</title>
+    <!-- Bootstrap v5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<%=request.getContextPath()%>/estilos/registrar.css">
 </head>
 <body>
-<%--Registrar usuarios/perfiles en el sistema--%>
+<div class="centered-form">
+    <div class="shadowed-box">
+        <h1>Registro de Usuario</h1>
+
+        <form action="${pageContext.request.contextPath}/controlUsuario?action=registrar" method="post">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Contraseña:</label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+            <div class="mb-3">
+                <label for="rol" class="form-label">Rol:</label>
+                <select class="form-select" id="rol" name="rol">
+                    <option value="Administrador">Administrador</option>
+                    <option value="Cajero">Cajero</option>
+                    <option value="Almacenero">Almacenero</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="estado" class="form-label">Estado:</label>
+                <input type="text" class="form-control" id="estado" name="estado">
+            </div>
+	    <div class="text-center">
+            <button type="submit" class="btn btn-dark" >Registrar</button>
+	    </div>
+        </form>
+    </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>

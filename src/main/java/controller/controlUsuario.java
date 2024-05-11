3eca777322a6fa0c0c94efd28139dee536ee5c7f
RefinedCandle49 <valdezvargaskevin@gmail.com>
2024-05-10 23:41:22 -0500

    Cambios en controller para identificación de datos (inputs)

--- src/main/java/controller/controlUsuario.java
+++ src/main/java/controller/controlUsuario.java
@@ -59,6 +59,8 @@ public class controlUsuario extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        request.setCharacterEncoding("UTF-8");
+
         String action = request.getParameter("action");
 
         switch (action) {

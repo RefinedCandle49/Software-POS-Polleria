dd3f9b72a08edde0c1fdda6e3c1c4c2ccf768a06
DaikoDev <cdanieleon28@gmail.com>
2024-05-10 23:20:11 -0500

    Se elimino utf8

--- src/main/java/controller/controlUsuario.java
+++ src/main/java/controller/controlUsuario.java
@@ -59,8 +59,6 @@ public class controlUsuario extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        request.setCharacterEncoding("UTF-8");
-
         String action = request.getParameter("action");
 
         switch (action) {
